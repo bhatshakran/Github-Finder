634d5ad09353e7ad5e4ae121d3120875f4fa1241
bhatshakran <bhatshakran@gmail.com>
2023-03-19 12:31:43 +0530

    Changed env vars

--- github.js
+++ github.js
@@ -1,13 +1,11 @@
 class Github {
   constructor() {
-    this.client_id = "351e55b6986b000f47af";
-    this.client_secret = "08f6a48f8664f4d3c1a9f37c95e92248689341c9";
     this.repos_count = 5;
     this.repos_sort = "created: asc";
   }
   async getUser(user) {
     const responseProfile = await fetch(`https://api.github.com/users/${user}
-        ?client_id = ${this.client_id}&client_secret = ${this.client_secret}`);
+        ?client_id = ${GITHUB_CLIENT_ID}&client_secret = ${GITHUB_CLIENT_SECRET}`);
 
     const repoResponse =
       await fetch(`https://api.github.com/users/${user}/repos?
